8f7b9e71a02881f1b0401de09a389627a5b17651
Liz McFarland <thelizmcfarland@gmail.com>
2024-03-04 15:14:07 -0800

    Set up react

---
+++ liz-web/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hi, I'm Liz.
+        </p>
+        <p>I'm a technical writer, currently writing docs at <a className="App-link" href="https://doc.lucidworks.com/" target="_blank" rel="noopener noreferrer"> Lucidworks</a>. I help run <a className="App-link" href="https://blog.crashspace.org/" target="_blank" rel="noopener noreferrer"> CRASH Space</a>, a Los Angeles-based hackerspace.
+        
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/liz-mcfarland-87a3a663/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/dianaprince"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
