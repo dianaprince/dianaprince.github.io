a3610fb95f13c92850065eb89205407c71702560
Liz McFarland <thelizmcfarland@gmail.com>
2024-03-05 13:59:12 -0800

    Improve design

--- liz-web/src/App.js
+++ liz-web/src/App.js
@@ -5,29 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hi, I'm Liz.
-        </p>
-        <p>I'm a technical writer, currently writing docs at <a className="App-link" href="https://doc.lucidworks.com/" target="_blank" rel="noopener noreferrer"> Lucidworks</a>. I help run <a className="App-link" href="https://blog.crashspace.org/" target="_blank" rel="noopener noreferrer"> CRASH Space</a>, a Los Angeles-based hackerspace.
-        
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/liz-mcfarland-87a3a663/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        <a
-          className="App-link"
-          href="https://github.com/dianaprince"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
+        <h1>
+          Hi, I'm Liz McFarland
+        </h1>
       </header>
+      <header className="App-header">
+        <h2>
+        I'm a technical writer, currently writing docs at <a className="App-link" href="https://doc.lucidworks.com/" target="_blank" rel="noopener noreferrer"> Lucidworks</a>.
+        </h2>
+      </header>
+      <section className="App-body">
+        <p>I help run <a className="App-link" href="https://blog.crashspace.org/" target="_blank" rel="noopener noreferrer"> CRASH Space</a>, a hackerspace in Los Angeles. I like making wearables projects, and I host a monthly wearables meetup.</p>
+        <p>You can find me on <a className="App-link" href="https://www.linkedin.com/in/liz-mcfarland-87a3a663/" target="_blank" rel="noopener noreferrer">LinkedIn</a>, <a className="App-link" href="https://bsky.app/profile/lizmcfarland.bsky.social" target="_blank" rel="noopener noreferrer">BlueSky</a>, and <a className="App-link" href="https://www.instagram.com/lizmcfa//" target="_blank" rel="noopener noreferrer">Instagram</a> and in the <a className="App-link" href="https://www.writethedocs.org/slack/" target="_blank" rel="noopener noreferrer">Write the Docs Slack</a>. You can also check out my <a className="App-link" href="https://github.com/dianaprince/" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+        </section>
     </div>
   );
 }
