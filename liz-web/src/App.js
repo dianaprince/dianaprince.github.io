d167c3b1ea8d4e292daf57c09b38d3776708cc09
Liz McFarland <thelizmcfarland@gmail.com>
2025-10-21 23:02:26 -0700

    Update job title and links

--- liz-web/src/App.js
+++ liz-web/src/App.js
@@ -11,12 +11,12 @@ function App() {
       </header>
       <header className="App-header">
         <h2>
-        I'm a technical writer, currently writing docs at <a className="App-link" href="https://doc.lucidworks.com/" target="_blank" rel="noopener noreferrer"> Lucidworks</a>.
+        I'm a technical program manager at <a className="App-link" href="https://doc.lucidworks.com/" target="_blank" rel="noopener noreferrer">Lucidworks</a>.
         </h2>
       </header>
       <section className="App-body">
         <p>I help run <a className="App-link" href="https://blog.crashspace.org/" target="_blank" rel="noopener noreferrer"> CRASH Space</a>, a hackerspace in Los Angeles. I like making wearables projects, and I host a monthly wearables meetup.</p>
-        <p>You can find me on <a className="App-link" href="https://www.linkedin.com/in/liz-mcfarland-87a3a663/" target="_blank" rel="noopener noreferrer">LinkedIn</a>, <a className="App-link" href="https://bsky.app/profile/lizmcfarland.bsky.social" target="_blank" rel="noopener noreferrer">BlueSky</a>, and <a className="App-link" href="https://www.instagram.com/lizmcfa/" target="_blank" rel="noopener noreferrer">Instagram</a> and in the <a className="App-link" href="https://www.writethedocs.org/slack/" target="_blank" rel="noopener noreferrer">Write the Docs Slack</a>. You can also check out my <a className="App-link" href="https://github.com/dianaprince/" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+        <p>You can find me on <a className="App-link" href="https://www.linkedin.com/in/liz-mcfarland-87a3a663/" target="_blank" rel="noopener noreferrer">LinkedIn</a>, <a className="App-link" href="https://bsky.app/profile/lizmcfarland.bsky.social" target="_blank" rel="noopener noreferrer">BlueSky</a>, and <a className="App-link" href="https://www.instagram.com/lizmcfa/" target="_blank" rel="noopener noreferrer">Instagram</a>.. You can also check out my <a className="App-link" href="https://github.com/dianaprince/" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
         </section>
     </div>
   );
